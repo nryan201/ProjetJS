75a095f0c8a96a689e3e239deee6849e880de19a
ALX-18 <148252959+ALX-18@users.noreply.github.com>
2024-04-14 21:12:57 +0200

    1/ fix the css bug     2/ add the style.css file     3/ update the package.json file     4/ update the index.html file     5/ update the pieces.js file     6/ update the chess.js file     7/ delete the chess.css file

---
+++ chess.js
@@ -0,0 +1,41 @@
+const gameBoard = document.getElementById('gameboard');
+const playerDisplay = document.getElementById('player');
+const infoDisplay = document.getElementById('info-display');
+const width = 8;
+
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
+]
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML = startPiece
+        square.setAttribute('square-id', i )
+        const row = Math.floor( (63 - i )/ 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown")
+        } else {
+            square.classList.add(i % 2 === 0 ? "brown" : "beige")
+        } 
+
+        if (i <= 15) {
+            square.firstChild.firstChild.classList.add("black")
+        }
+
+        if (i >= 48) {
+            square.firstChild.firstChild.classList.add("white")
+        }
+
+        gameBoard.appendChild(square)
+    })
+}
+createBoard()  
