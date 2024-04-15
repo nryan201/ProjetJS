98d745a21805497c699d2172a7a1ce1c0259dd69
Paul <paul.amalric@ynov.com>
2024-04-15 10:54:46 +0200

    calculateur vitesse de frape au clavier texte mis

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
-const quoteDisplayElement = document.getElementById('quoteDisplay');
-const quoteInputElement = document.getElementById('quoteInput');
-
-function getRandomQuote() {
-    fetch(RANDOM_QUOTE_API_URL)
-        .then(response => response.json())
-        .then(data => data.content)
+// Fonction pour charger un fichier texte
+function File(texte_cvfc, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", texte_cvfc, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send();
 }
 
-async function renderNewQuote() {
-    const quote = await getRandomQuote()
-    console.log(quote)
-    quoteInputElement.value = null;
+// Fonction pour choisir une phrase au hasard dans le texte
+function sentence(texte_cvfc) {
+    var phrases = texte_cvfc.split("\n"); // Séparer les phrases par retour à la ligne
+    var indice = Math.floor(Math.random() * phrases.length); // Choisir un indice au hasard
+    return phrases[indice];
 }
 
-renderNewQuote();
+// Utilisation : charger le fichier texte et choisir une phrase au hasard
+File("texte_cvfc.txt", function (texte_cvfc) {
+    var phrase = sentence(texte_cvfc);
+    document.getElementById("phrase").textContent = phrase; // Mettre à jour le contenu de l'élément HTML avec la phrase choisie
+});
+
+
+function characterArray (){
+    phrase.split('').forEach(character => {
+        const characterSpan = document.createElement("span");
+        characterSpan.innerText = character;
+    })
+}
